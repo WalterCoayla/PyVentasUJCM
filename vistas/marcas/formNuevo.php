565b79fa0c5d6d5d806a37f3c8f7bf96c07aa79b
Walter Coayla <walter.coayla@gmail.com>
2023-07-01 15:54:58 -0500

    Primer Commit

--- vistas/marcas/formNuevo.php
+++ vistas/marcas/formNuevo.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Marcas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
@@ -14,6 +14,7 @@
     # var_dump($datos);
     $id = isset($datos['idmarcas'])?$datos['idmarcas']:'';
     $nombre= isset($datos['nombre'])?$datos['nombre']:'';
+    
     $editar = ($id!='')?1:0;
     $titulo= ($editar==1)?'Editar Marca':'Nueva Marca';
     ?>
